10fa89853dfefab82bd2840d0de7919afd356f6f
panshul.gupta <panshul.gupta@masterpayment.com>
2017-11-10 09:44:34 +0100

    making it work.

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react'
 import '../styles/Chat.css'
 import ChatInput from './ChatInput'
 import ChatMessages from './ChatMessages'
+import { graphql, compose } from 'react-apollo'
+import gql from 'graphql-tag'
 
 class Chat extends Component {
 
   state = {
     message: '',
-    allMessages: []
   }
 
   render() {
     return (
       <div className='Chat'>
         <ChatMessages
-          messages={this.state.allMessages || []}
+          messages={this.props.allMessagesQuery.allMessages || []}
           endRef={this._endRef}
         />
         <ChatInput
@@ -29,9 +30,12 @@ class Chat extends Component {
 
   _onSend = () => {
     console.log(`Send: ${this.state.message}`)
-    const newMessages = this.state.allMessages
-    newMessages.push({ text: this.state.message })
-    this.setState({allMessages: newMessages})
+    this.props.createMessageMutation({
+      variables: {
+        text: this.state.message,
+        sentById: this.props.userId
+      }
+    })
   }
 
   _resetText = () => {
@@ -54,6 +58,69 @@ class Chat extends Component {
     }
   }
 
+  componentDidMount() {
+    this.createMessageSubscription = this.props.allMessagesQuery.subscribeToMore({
+      document: NEW_MESSAGE_SUBSCRIPTION,
+      updateQuery: (previousState, {subscriptionData}) => {
+        console.log(`Received message: ${subscriptionData.Message.node.text}`)
+        const newMessage = subscriptionData.Message.node
+        const messages = previousState.allMessages.concat([newMessage])
+        return {
+          allMessages: messages
+        }
+      },
+      onError: (err) => console.error(err),
+    })
+  }
+
 }
 
-export default Chat
+const ALL_MESSAGES_QUERY = gql`
+  query {
+    allMessages(last:100) {
+      id
+      text
+      createdAt
+      sentBy {
+        id
+        name
+      }
+    }
+  }
+  `
+  const NEW_MESSAGE_SUBSCRIPTION = gql`
+  subscription NewMessageSubscription {
+    Message(filter: {
+      mutation_in: [CREATED]
+    }) {
+      node {
+        id
+        text
+        createdAt
+        sentBy {
+          id
+          name
+        }
+      }
+    }
+  }
+  `
+
+const CREATE_MESSAGE_MUTATION = gql`
+  mutation CreateMessageMutation($text: String!, $sentById: ID!) {
+    createMessage(text: $text, sentById: $sentById) {
+      id
+      text
+      createdAt
+      sentBy {
+        id
+        name
+      }
+    }
+  }
+`
+
+export default compose(
+  graphql(CREATE_MESSAGE_MUTATION, {name:'createMessageMutation'}),
+  graphql(ALL_MESSAGES_QUERY, {name:'allMessagesQuery'})
+)(Chat);
--- src/index.js
+++ src/index.js
@@ -2,8 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import './styles/index.css'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient, InMemoryCache, HttpLink, gql } from 'apollo-client-preset'
+
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
+const serviceId = 'cj9su1gki8qn00182h37oa733'
+const httpLink = new HttpLink({uri: `https://api.graph.cool/simple/v1/${serviceId}`})
+
+const wsLink = new WebSocketLink({
+  uri: `wss://subscriptions.graph.cool/v1/${serviceId}`,
+  options: {
+    reconnect: true
+  }
+})
+
+const isSubscription = gqlOperation => {
+  const { kind, operation } = getMainDefinition(gqlOperation.query)
+  return kind === 'OperationDefinition' && operation === 'subscription'
+}
+
+const link = split(
+  isSubscription,
+  wsLink,
+  httpLink,
+)
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+})
+
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+  <App />
+  </ApolloProvider>,
   document.getElementById('root')
 )
